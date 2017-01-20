4e0a5f09a5774c4f6c5c07664a6f2d471d818130
Evandro C Cortiano <evandrocortiano@hotmail.com>
2017-01-19 22:18:26 -0200

    Segundo Commit

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,15 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'title' => $faker->sentence,
+        'content' => $faker->paragraph,
     ];
+});
+
+$factory->define(App\Tag::class, function (Faker\Generator $faker) {
+	return [
+			'name' => $faker->word
+	];
 });
